ac39ac8ad6374c31f49beda7dd6fcece313b9c8b
laishao <280826244@qq.com>
2024-03-11 23:39:29 +0800

    【二分】局部最小值问题

---
+++ src/class01/Code06_BSAwesome_copy.java
@@ -0,0 +1,31 @@
+package src.class01;
+
+public class Code06_BSAwesome_copy {
+
+	public static int getIndex(int[] arr) {
+		if (arr == null || arr.length < 1) {
+			return -1;
+		}
+		if (arr.length == 1 || arr[0] < arr[1]) {
+			return 0;
+		}
+		int n = arr.length;
+		if (arr[n - 1] < arr[n - 2]) {
+			return n - 1;
+		}
+		int L = 1;
+		int R = n - 2;
+		while(L < R) {
+			int mid = L + ((R - L) >> 1);
+			if (arr[mid] > arr[mid - 1]) {
+				R = mid - 1;
+			} else if(arr[mid] > arr[mid + 1]) {
+				L = mid + 1;
+			} else {
+				return mid;
+			}
+		}
+		return L;
+	}
+
+}
